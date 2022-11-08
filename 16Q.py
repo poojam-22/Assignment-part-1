b0e98e925688d74681718e71e224e8e314d188ac
poojam-22 <117710998+poojam-22@users.noreply.github.com>
2022-11-08 11:55:34 +0530

    Add files via upload

---
+++ 16Q.py
@@ -0,0 +1,4 @@
+m='iNeuron'*3
+print(" ' ' ' " ,end= " ")
+print(m,end= " ")
+print(" ' ' ' ",end=" ")
---
+++ 22Q.py
@@ -0,0 +1,5 @@
+age=int(input(" enter the age"))
+if(age<=18):
+    print("i can't vote")
+else:
+    print("i can vote")
